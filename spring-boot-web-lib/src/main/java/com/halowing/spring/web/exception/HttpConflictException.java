76ba0171afe26828d49f2b50f130c91508384af2
김성근 <sgkim@aircode.com>
2022-04-12 21:45:27 +0900

    ID 충돌용 Exception 추가

---
+++ spring-boot-web-lib/src/main/java/com/halowing/spring/web/exception/HttpConflictException.java
@@ -0,0 +1,17 @@
+package com.halowing.spring.web.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class HttpConflictException extends DefaultApplicationException  {
+
+	private static final long serialVersionUID = -1702667171228990922L;
+	
+	public HttpConflictException() {
+		super(HttpStatus.CONFLICT);
+	}
+	
+	public HttpConflictException(String id) {
+		super(HttpStatus.CONFLICT,"ID is "+id+". and It is conflict.");
+	}
+
+}
