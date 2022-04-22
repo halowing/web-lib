e0ed2c2154d6ae1baa49ac5a9ded2d63b9d15a60
김성근 <sgkim@aircode.com>
2022-04-22 11:41:08 +0900

    cross domain 시 header 변경 되는 부분 피하기 위해 , OPTION 부분 따로 처리

--- spring-boot-web-lib/src/main/java/com/halowing/servlet/filter/apikey/ApiKeyFilter.java
+++ spring-boot-web-lib/src/main/java/com/halowing/servlet/filter/apikey/ApiKeyFilter.java
@@ -10,12 +10,17 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.HttpClientErrorException;
 
 public class ApiKeyFilter implements Filter{
 	
-	private final static String HEADER_NAME = "api-key";
+	private final Logger log = LoggerFactory.getLogger(ApiKeyFilter.class);
+	
+	private final static String HEADER_NAME = "Api-key";
 	
 	private final ApiKeyService service;
 	
@@ -29,29 +34,43 @@ public class ApiKeyFilter implements Filter{
 			throws IOException, ServletException {
 		
 		HttpServletRequest httpRequest = (HttpServletRequest) request;
-		
-		Enumeration<String> headers = httpRequest.getHeaderNames();
-		String headerName = null;
-		String value = null;
-		while(headers.hasMoreElements()) {
-			headerName = headers.nextElement();
+		log.debug("Method = {}", httpRequest.getMethod());
+		if(HttpMethod.OPTIONS.name().equals(httpRequest.getMethod())) {
+			log.debug("OPTION");
+		}else {
+			Enumeration<String> headers = httpRequest.getHeaderNames();
 			
-			if(HEADER_NAME.equalsIgnoreCase(headerName))
-			{
+			String headerName = null;
+			String value = null;
+			
+			while(headers.hasMoreElements()) {
+				headerName = headers.nextElement();
 				value = httpRequest.getHeader(headerName);
-				break;
+				
+				log.debug("header name = {}, \t value = {}", headerName, value);
+				
+				if(HEADER_NAME.equalsIgnoreCase(headerName))
+				{
+					break;
+				}
+				else
+				{
+					headerName = null;
+					value = null;
+				}
 			}
-			else
-				headerName = null;
-		}
-		
-		if(headerName == null || value == null || value.isBlank()  )
-			throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED,"api_key is not present.");
 			
-		if(!service.hasApiKey(value))
-//			throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED,"It is not granted to access this system.\n Api-key="+value);
-			throw new ServletException("It is not granted to access this system.\n Api-key="+value);
-		
+			
+			
+			if(headerName == null )
+				throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED," Api-key is not present ");
+			
+			if( value == null || value.isBlank()  )
+				throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED,"value of Api-key is empty.");
+			
+			if(!service.hasApiKey(value))
+				throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED,"It is not granted to access this system. Api-key="+value);
+		}
 		chain.doFilter(request, response);
 	}
 
