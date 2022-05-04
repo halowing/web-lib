ffb368e23ca285d34739b77ce843477bd73b0e98
김성근 <sgkim@aircode.com>
2022-05-05 01:34:34 +0900

    String을 반올림 해주는 함수 추가

--- spring-boot-web-lib/src/main/java/com/halowing/util/NumberUtility.java
+++ spring-boot-web-lib/src/main/java/com/halowing/util/NumberUtility.java
@@ -2,13 +2,30 @@ package com.halowing.util;
 
 public class NumberUtility {
 	
-	public static Float round(Float number, int position) {
+	public static Float round(Float number, int offset) {
 		if(number == null) return null;
 		
-		String pow = "" + Math.pow(10, position);
+		int multiple = 1;
+		for(int i=0;i<offset;i++) {
+			multiple *= 10;
+		}
 		
-		return Math.round(number * Float.valueOf(pow)) / Float.valueOf(pow); 
+		return  Math.round(number * multiple) / (multiple * 1f);
 		
+		
+	}
+	
+	public static String round(String number, int offset, String defaultValue) {
+		
+		if(StringUtility.isBlank(number)) return defaultValue;
+		
+		if(!number.contains("."))
+			return number;
+		
+		float a = Float.valueOf(number) ;
+	
+		
+		return "" +  round(a,offset);
 	}
 
 }
