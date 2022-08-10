cac2b8b7ebed2a51747b6d7589a38a2d33faba69
sgkim <sgkim@aircode.com>
2022-08-10 14:21:39 +0900

    ErrorCode를 enum 에서 string으로 변경

--- spring-boot-web-lib/src/main/java/com/halowing/spring/web/code/ErrorCode.java
+++ spring-boot-web-lib/src/main/java/com/halowing/spring/web/code/ErrorCode.java
@@ -1,22 +1,13 @@
 package com.halowing.spring.web.code;
 
-public enum ErrorCode {
+public class ErrorCode {
 
-	NOT_FOUND					("response.error.notfound"), 
-	MEMBER_NOT_FOUND			("response.error.member.notfound"), 
-	DUPLICATED_LOGINID			("response.error.member.loginId.duplicated"),
-	SECURITY_INVALID			("response.error.security.invalid"), 
-	EMPTY_INPUT					("response.error.input.empty"),
+	public static final String ETC					= 	"response.error.etc";
+	public static final String NOT_FOUND			=	"response.error.notfound";
+	public static final String SECURITY_INVALID		=	"response.error.security.invalid"; 
+	public static final String EMPTY_INPUT			=	"response.error.input.empty";
 	
-	;
+	public static final String MEMBER_NOT_FOUND		=	"response.error.member.notfound"; 
+	public static final String DUPLICATED_LOGINID	=	"response.error.member.loginId.duplicated";
 	
-	String code;
-	
-	ErrorCode(String code){
-		this.code = code;
-	}
-	
-	public String getCode() {
-		return code;
-	}
 }
