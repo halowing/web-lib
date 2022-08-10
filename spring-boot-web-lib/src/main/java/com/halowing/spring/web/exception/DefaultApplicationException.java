33b06a5010a514b3e97d3118b71064cf6f5ac58d
sgkim <sgkim@aircode.com>
2022-08-10 14:32:18 +0900

    응답 메시지 처리를 위한 code 와 arts[] 파라미터 추가

--- spring-boot-web-lib/src/main/java/com/halowing/spring/web/exception/DefaultApplicationException.java
+++ spring-boot-web-lib/src/main/java/com/halowing/spring/web/exception/DefaultApplicationException.java
@@ -1,48 +1,75 @@
 package com.halowing.spring.web.exception;
 
+import java.util.Arrays;
+
 import org.springframework.http.HttpStatus;
+
+import com.halowing.spring.web.code.ErrorCode;
+import com.halowing.util.StringUtility;
 
 public class DefaultApplicationException extends RuntimeException {
 
 	private static final long serialVersionUID = -8732186396685769772L;
 	
-	private static final HttpStatus DEFAULT_ERROR_STATUS = HttpStatus.INTERNAL_SERVER_ERROR;
+	private String code = ErrorCode.ETC;
+	private String[] args;
 	
-	private HttpStatus status;
-//	private String message;
-	
-
+	private HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
 
 	public DefaultApplicationException() {
-		this.status = DEFAULT_ERROR_STATUS;
+		this(null, null , null);
 	}
 	
 	public DefaultApplicationException(String message) {
-		super(message);
-		this.status = DEFAULT_ERROR_STATUS;
+		this(null, message, null );
 	}
 	
 	public DefaultApplicationException(HttpStatus status) {
-		this(status,null);
+		this(status, null, null );
 	}
 
 	public DefaultApplicationException(HttpStatus status, String message) {
-		super(message == null? status.getReasonPhrase() :message);
-		this.status = status;
+		this(status, message, null );
 	}	
 
-	public DefaultApplicationException(Throwable cause) {
-		super(cause);
-		this.status = DEFAULT_ERROR_STATUS;
+	public DefaultApplicationException(HttpStatus status, String message, String code, String... args) {
+		super(message == null? status.getReasonPhrase() :message);
+		
+		if(status != null)
+			this.status = status ;
+		
+		if(StringUtility.isBlank(code))
+			this.code = code;
+		
+		this.args = args;
 	}
 	
+	public DefaultApplicationException(String message ,Throwable cause) {
+		super(message,cause);
+	}
+	
+	public DefaultApplicationException(Throwable cause) {
+		super(cause);
+	}
+	
+	@Override
+	public String toString() {
+		return "DefaultApplicationException [code=" + code + ", args=" + Arrays.toString(args) + ", status=" + status
+				+ "]";
+	}
+
 	public HttpStatus getStatus() {
 		return this.status;
 	}
-
-	@Override
-	public String toString() {
-		return "DefaultApplicationException [status=" + status + "]";
+	
+	public String getCode() {
+		return this.code;
 	}
+	
+	public String[] getArgs() {
+		return this.args;
+	}
+
+	
 	
 }
