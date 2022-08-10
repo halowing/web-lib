8780ebe03e36a1f6bf5f305aeddee6dd3965a29a
sgkim <sgkim@aircode.com>
2022-08-10 18:43:52 +0900

    사용하지 않는 라이브러리 제거

--- spring-boot-web-lib/src/main/java/com/halowing/spring/web/resolver/DefaultApplicationExceptionResolver.java
+++ spring-boot-web-lib/src/main/java/com/halowing/spring/web/resolver/DefaultApplicationExceptionResolver.java
@@ -3,21 +3,14 @@ package com.halowing.spring.web.resolver;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.MessageSource;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.AbstractHandlerExceptionResolver;
 
-import com.halowing.spring.web.code.ErrorCode;
 import com.halowing.spring.web.exception.DefaultApplicationException;
 
 public class DefaultApplicationExceptionResolver extends AbstractHandlerExceptionResolver {
-	
-	private static final Logger log = LoggerFactory.getLogger(DefaultApplicationExceptionResolver.class);
 	
 	private final LocaleResolver localeResolver;
 	private final MessageSource messageSource;
