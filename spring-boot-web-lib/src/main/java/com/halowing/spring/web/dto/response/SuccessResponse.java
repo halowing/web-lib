d70f3725659d633e49181df8cc117962d848afe8
sgkim <sgkim@aircode.com>
2022-08-10 14:25:15 +0900

    message를 httpStatus 상태 메시지로 대체

--- spring-boot-web-lib/src/main/java/com/halowing/spring/web/dto/response/SuccessResponse.java
+++ spring-boot-web-lib/src/main/java/com/halowing/spring/web/dto/response/SuccessResponse.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 public class SuccessResponse extends DefaultResponse{
 
 	public SuccessResponse() {
-		super(HttpStatus.OK,"OK");
+		super(HttpStatus.OK,HttpStatus.OK.getReasonPhrase());
 	}
 
 	private static final long serialVersionUID = -8918304779280171220L;
