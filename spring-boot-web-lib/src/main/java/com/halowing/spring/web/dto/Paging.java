feab6852d1d09672b45fe7a8d2937cd60d200f5c
sgkim <sgkim@aircode.com>
2022-10-31 15:56:33 +0900

    validation 조건 추가. 생성자 추가.

--- spring-boot-web-lib/src/main/java/com/halowing/spring/web/dto/Paging.java
+++ spring-boot-web-lib/src/main/java/com/halowing/spring/web/dto/Paging.java
@@ -1,5 +1,10 @@
 package com.halowing.spring.web.dto;
 
+import java.io.Serializable;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
@@ -9,14 +14,30 @@ import io.swagger.annotations.ApiModelProperty;
  *
  */
 @ApiModel(description = "페이징 객체")
-public class Paging {
+public class Paging implements Serializable {
+	
+	private static final long serialVersionUID = -4779942866088281302L;
 
 	@ApiModelProperty(value = "검색할 컨텐트의 시작 위치, 첫페이지는 0 , offset = (page-1)*limit")
+	@NotNull
+	@Min(value = 0)
 	private Integer offset;
 	
 	@ApiModelProperty(value = "검색할 컨텐트의 갯 수")
+	@NotNull
+	@Min(value = 1)
 	private Integer limit;
 	
+	
+	public Paging() {
+		
+	}
+	
+	public Paging(@NotNull @Min(0) Integer offset, @NotNull @Min(1) Integer limit) {
+		this.offset = offset;
+		this.limit = limit;
+	}
+
 	@Override
 	public String toString() {
 		return "Paging [offset=" + offset + ", limit=" + limit + "]";
